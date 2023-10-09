ef55eaffda0698da9abfe9454813c6801e1ab7de
Ibrahim Ouhamou <ouhamouyare10@gmail.com>
2023-10-09 13:14:59 +0100

    BismiAllah setting up the server header

--- src/http_server.c
+++ src/http_server.c
@@ -1,9 +1,76 @@
 //بسم الله الرحمن لارحيم
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
 #include "http_server.h"
 
-void init_Server(HTTP_Server *http_server, int port)
+int init_Server(HTTP_Server *http_server, int port)
 {
-  
+    http_server->port = port;
+
+    http_server->socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (0 > http_server->socket)
+    {
+        perror("ERROR: socket()\n");
+        return -1;        
+    }
+
+    struct sockaddr_in s_addr;
+    bzero((char*)&s_addr, sizeof(s_addr));
+    s_addr.sin_family = AF_INET;
+    s_addr.sin_addr.s_addr = INADDR_ANY;
+    s_addr.sin_port = htons(http_server->port);
+    if(0 > bind(http_server->socket, &s_addr, sizeof(s_addr)))
+    {
+        perror("ERROR: bind()\n");
+        return -1;
+    }
+    return 0;
 }
 
+int connect_server(HTTP_Server *http_server, int *cli_fd)
+{
+  if(0 > listen(http_server->socket, 5))
+  {
+    perror("ERROR: listen()\n");
+    return -1;
+  }
+
+  socklen_t cli_len = sizeof(*cli_fd);
+  *cli_fd = accept(http_server->socket, (struct sockaddr*)(cli_fd), &cli_len);
+  if(0 > *cli_fd)
+  {
+    perror("ERROR: accept()\n");
+    return -1;
+  }
+
+  return 0;
+}
+
+int establish_connection(int *sockfd, int *cli_sockfd)
+{
+  //listen
+  int n = listen(*sockfd, 5);
+  if(n < 0)
+  {
+    perror("ERRROR: listen\n");
+    return -1;
+  }
+
+  //accept
+  socklen_t cli_len = sizeof(*cli_sockfd);
+  *cli_sockfd = accept(*sockfd, (struct sockaddr*)(cli_sockfd), &cli_len);
+  if(0 > *cli_sockfd)
+  {
+    perror("ERROR: accept\n");
+    return -1;
+  }
+   return 0;
+}
--- src/http_server.h
+++ src/http_server.h
@@ -1,10 +1,14 @@
 //بسم الله الرحمن لارحيم
 
+#ifndef HTTP_SERVER_H
+#define HTTP_SERVER_H
 typedef struct HTTP_Server
 {
   int socket;
   int port;
 } HTTP_Server;
 
-void init_Server(HTTP_Server *http_server, int port);
+int init_Server(HTTP_Server *http_server, int port);
+int connect_server(HTTP_Server *Http_server, int cli_fd);
 
+#endif
